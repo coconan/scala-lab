5b981b48e132d092e11859fd25d2c6b1f60f7579
coconan <coconan8@gmail.com>
2023-02-27 21:59:52 +0800

    handsonscala - ajax

---
+++ cask/app/resources/static/app.js
@@ -0,0 +1,11 @@
+function submitForm() {
+    fetch("/", {method: "POST", body: JSON.stringify({name: nameInput.value, msg: msgInput.value})})
+    .then(response => response.json())
+    .then(json => {
+        if (json["success"]) {
+            messageList.innerHTML = json["txt"]
+            msgInput.value = ""
+        }
+        errorDiv.innerText = json["err"]
+    })
+}
