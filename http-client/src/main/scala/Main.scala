334b2f2ee8e70fb9b676e794f302518c59d22088
coconan <coconan.by@gmail.com>
2023-08-10 20:54:59 +0800

    hack scala - http-client

---
+++ http-client/src/main/scala/Main.scala
@@ -0,0 +1,19 @@
+import sttp.client4._
+
+object Main extends App {
+  val sort: Option[String] = None
+  val query = "http language:scala"
+
+  // the `query` parameter is automatically url-encoded
+  // `sort` is removed, as the value is not defined
+  val request = basicRequest.get(uri"https://api.github.com/search/repositories?q=$query&sort=$sort")
+    
+  val backend = DefaultSyncBackend()
+  val response = request.send(backend)
+
+  // response.header(...): Option[String]
+  println(response.header("Content-Length")) 
+
+  // response.body: by default read into an Either[String, String] to indicate failure or success 
+  println(response.body)  
+}
